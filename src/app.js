e201558585476738d4d2a2c9d81506c8f02bf8c6
Ranjan Kumar <ranjan73.me@gmail.com>
2025-11-22 17:34:54 +0530

    update app with all routes

--- src/app.js
+++ src/app.js
@@ -21,9 +21,25 @@ app.use(cookieParser());
 //import routes
 import userRouter from "./routes/user.route.js";
 import profileRouter from "./routes/profile.route.js";
+import channelRouter from "./routes/channel.route.js";
+import videoRouter from "./routes/video.route.js";
+import subscriptionRouter from "./routes/subscription.route.js";
+import playlistRouter from "./routes/playlist.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
+import likeRouter from "./routes/like.route.js";
+import activityRouter from "./routes/activity.route.js";
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/profiles", profileRouter);
+app.use("/api/v1/channels", channelRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/activities", activityRouter);
 
 app.use((req, res, next) => {
     next(new ApiError(404, `Route ${req.originalUrl} not found!`));
